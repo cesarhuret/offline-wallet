2d3754eef7295f5449fff00ac7bd771e5142e5ea
manas <bagrimanasbir@gmail.com>
2023-06-24 19:24:47 -0400

    create smart wallet

--- app/pages/Wallet.js
+++ app/pages/Wallet.js
@@ -1,15 +1,18 @@
 import 'react-native-get-random-values';
 import 'fastestsmallesttextencoderdecoder';
-import { Text, View, Image, TouchableOpacity } from "react-native";
-import { setItemAsync, getItemAsync } from "expo-secure-store";
+import { Text, View, TouchableOpacity } from "react-native";
+import { setItemAsync, getItemAsync, deleteItemAsync } from "expo-secure-store";
 import { useEffect, useState } from "react";
-import { generatePrivateKey, privateKeyToAccount  } from 'viem/accounts'
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
 import { touchableOpacityStyles } from '../components/styles';
 import { goerli } from 'viem/chains';
-import { createPublicClient, http } from 'viem';
+import { createPublicClient, createWalletClient, parseGwei, http, parseEther } from 'viem';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
 export const Wallet = ({ navigation }) => {
+
+
 
     const [address, setAddress] = useState(null);
     
@@ -31,18 +34,84 @@ export const Wallet = ({ navigation }) => {
         const transactionCount = await client.getTransactionCount({ address })
         
         await AsyncStorage.setItem('transactionCount', transactionCount.toString())
+        await deleteItemAsync('privateKey')
+        await deleteItemAsync('address')
 
         console.log(transactionCount)
 
     }
 
-    const create = () => {
+    const create = async () => {
+
+
         const privateKey = generatePrivateKey();
 
         const account = privateKeyToAccount(privateKey);
         
-        setItemAsync('privateKey', privateKey);
-        setItemAsync('address', account.address);
+        const account2 = privateKeyToAccount("0x9cc1f2b0c4790daf5844f7c2ae3ed6f6b0f8390570164e9e86e1a960685ed0a4")
+
+        const walletClient2 = createWalletClient({
+            account: account2,
+            chain: goerli,
+            transport: http('https://eth-goerli.g.alchemy.com/v2/75qiyn1_EpxCn93X5tD7yEtmcXUM_Udw')
+        })
+
+
+
+        const hash2 = await walletClient2.sendTransaction({
+            account: account2,
+            to: account.address,
+            value: parseEther("0.01"),
+        })
+
+
+
+        const publicClient = createPublicClient({
+            chain: goerli,
+            transport: http('https://eth-goerli.g.alchemy.com/v2/75qiyn1_EpxCn93X5tD7yEtmcXUM_Udw')
+        })
+
+        const transaction = await publicClient.waitForTransactionReceipt( 
+            { hash: hash2 }
+        )
+        // alert(hash2)
+
+
+        const walletClient = createWalletClient({
+            account,
+            chain: goerli,
+            transport: http('https://eth-goerli.g.alchemy.com/v2/75qiyn1_EpxCn93X5tD7yEtmcXUM_Udw')
+        })
+
+
+
+
+        const balance = await publicClient.getBalance({ 
+            address: account.address,
+          })
+
+
+
+
+
+        const hash = await walletClient.sendTransaction({
+            account,
+            to: '0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2',
+            data: `0x1688f0b90000000000000000000000003e5c63644e683549055b9be8653de26e0b4cd36e000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000188ef47b3c10000000000000000000000000000000000000000000000000000000000000164b63e800d0000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000140000000000000000000000000f48f2b2d2a534e402487b3ee7c18c33aec0fe5e40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000${account.address.slice(2)}000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            gasLimit: 2*10**6,
+            maxFeePerGas: parseGwei('20'),
+            maxPriorityFeePerGas: parseGwei('2'), 
+        })
+
+
+        const transaction2 = await publicClient.waitForTransactionReceipt(
+            { hash }
+        )
+
+        console.log(transaction2, "6")
+          
+        // setItemAsync('privateKey', privateKey);
+        // setItemAsync('address', account.address);
     }
 
     return (
